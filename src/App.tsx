6aefaad03ec89a22952554fbe864704e484d462c
angel <angel.munoz@tokiota.com>
2022-05-20 09:27:07 +0200

    use profiler react feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { Profiler } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Router, Header } from './core/components';
 
 function App() {
+  const onRenderCallback = (
+    id: string,
+    phase: unknown,
+    actualDuration: unknown,
+    baseDuration: unknown,
+    startTime: unknown,
+    commitTime: unknown,
+    interactions: unknown,
+  ) => { };
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Router />
-    </BrowserRouter>
+    <Profiler id="profilerApp" onRender={onRenderCallback}>
+      <BrowserRouter>
+        <Header />
+        <Router />
+      </BrowserRouter>
+    </Profiler>
   );
 }
 
