ded6b48a4819fc2a6df1cebf0df8ade0769185d9
angel <angel.munoz@tokiota.com>
2022-08-19 18:32:35 +0200

    install and integration infinite scroll react

--- src/warriors/pages/list/WarriorListPage.tsx
+++ src/warriors/pages/list/WarriorListPage.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 import styled from 'styled-components';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-import { warriorsService } from '../../services/warriors.service';
+import { warriorsService } from '../../services';
 import { WarriorInterface } from '../../interfaces';
 import { WarriorCard, WarriorFilter } from '../../components';
 import { Spinner } from '../../../shared/components';
+import { PageResponseInterface } from '../../../shared/interfaces';
 
 const PageContainer = styled.section`
   padding: 1.5rem;
@@ -30,16 +32,16 @@ const CardsContainer = styled.div`
 `;
 
 export default function WarriorListPage(): JSX.Element {
-  const [originalWarriors, setOriginalWarriors] = useState<WarriorInterface[]>();
   const [filterWarriors, setFilterWarriors] = useState<WarriorInterface[]>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [pageResponse, setPageResponse] = useState<PageResponseInterface<WarriorInterface>>();
 
   const getList = async () => {
     setIsLoading(true);
     try {
       const response = await warriorsService.getPeople();
+      setPageResponse(response);
       setIsLoading(false);
-      setOriginalWarriors(response.results);
       setFilterWarriors(response.results);
     } catch {
       // TODO show alert o some feature about error
@@ -47,8 +49,13 @@ export default function WarriorListPage(): JSX.Element {
     }
   };
 
-  const handleFilterWarriors = (Warriors: WarriorInterface[]) => {
-    setFilterWarriors(Warriors);
+  const handleFilterWarriors = (warriors: WarriorInterface[]) => {
+    setFilterWarriors(warriors);
+  };
+
+  const handleNextWarriors = () => {
+    console.log('handleNextWarriors');
+    getList();
   };
 
   useEffect(() => {
@@ -58,22 +65,30 @@ export default function WarriorListPage(): JSX.Element {
   return (
     <PageContainer>
       <h4>Warriors List</h4>
-      {!isLoading && (
+      {!isLoading && pageResponse && (
         <>
           <WarriorFilter
-            originalWarriors={originalWarriors}
+            originalWarriors={pageResponse.results}
             filterWarriors={filterWarriors}
-            handleSetDefault={() => { if (originalWarriors) setFilterWarriors([...originalWarriors]); }}
+            handleSetDefault={() => { if (pageResponse.results) setFilterWarriors([...pageResponse.results]); }}
             handleSortByTitle={setFilterWarriors}
             handleFilterColorHair={handleFilterWarriors}
           />
           <CardsContainer>
-            {filterWarriors && filterWarriors.length > 0 && filterWarriors.map((warrior) => (
-              <WarriorCard
-                key={warrior.name}
-                warrior={warrior}
-              />
-            ))}
+            <InfiniteScroll
+              dataLength={pageResponse?.count}
+              next={handleNextWarriors}
+              hasMore={true}
+              loader={<h4>Loading...</h4>}
+              style={{ width: '100%' }}
+            >
+              {filterWarriors && filterWarriors.length > 0 && filterWarriors.map((warrior) => (
+                <WarriorCard
+                  key={warrior.name}
+                  warrior={warrior}
+                />
+              ))}
+            </InfiniteScroll>
           </CardsContainer>
         </>
       )}
